4f31701ff35dcd6e6967a54e28242a6ade55ede5
Marcelo Oliveira <mclricardo@gmail.com>
2018-06-19 19:00:22 -0300

    913-CertificacaoCSharpParte1-video1.4-Números Integrais.trec

--- certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/2 - Tipos Inteiros/Inteiros.cs
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/2 - Tipos Inteiros/Inteiros.cs
@@ -43,25 +43,25 @@ namespace certificacao_csharp_roteiro
 
 
 
-            ///nivelDeAzul = 0xFFFF; // = 65535 em decimal
-            ///passageirosVoo = 230000; // System.Int16 
-            ///populacao = 15000000000; // System.Int32 
-            ///ushort passageirosNavio = -15; // System.UInt16 
-            ///uint estoque = -2300; // System.UInt32 
-            ///long populacaoDoBrasil = 207_660_929.345;
+            //nivelDeAzul = 0xFFFF; // = 65535 em decimal
+            //passageirosVoo = 230000; // System.Int16 
+            //populacao = 15000000000; // System.Int32 
+            //ushort passageirosNavio = -15; // System.UInt16 
+            //uint estoque = -2300; // System.UInt32 
+            //long populacaoDoBrasil = 207_660_929.345;
 
-            ///Tipo    Faixa de valores                    Tamanho
-            ///======================================================================
-            ///sbyte  -128 to 127                          Com sinal 8-bit integer
-            ///byte    0 to 255                            Sem sinal 8-bit integer
-            ///char    U+0000 to U+ffff                    Unicode 16-bit character
-            ///short  -32,768 to 32,767                    Com sinal 16-bit integer
-            ///ushort  0 to 65,535                         Sem sinal 16-bit integer
-            ///int     -2,147,483,648 to 2,147,483,647     Com sinal 32-bit integer
-            ///uint    0 to 4,294,967,295                  Sem sinal 32-bit integer
-            ///long    -9,223,372,036,854,775,808 
-            ///            to 9,223,372,036,854,775,807    Com sinal 64-bit integer
-            ///ulong   0 to 18,446,744,073,709,551,615     Sem sinal 64-bit integer
+            //Tipo    Faixa de valores                    Tamanho
+            //======================================================================
+            //sbyte  -128 to 127                          Com sinal 8-bit integer
+            //byte    0 to 255                            Sem sinal 8-bit integer
+            //char    U+0000 to U+ffff                    Unicode 16-bit character
+            //short  -32,768 to 32,767                    Com sinal 16-bit integer
+            //ushort  0 to 65,535                         Sem sinal 16-bit integer
+            //int     -2,147,483,648 to 2,147,483,647     Com sinal 32-bit integer
+            //uint    0 to 4,294,967,295                  Sem sinal 32-bit integer
+            //long    -9,223,372,036,854,775,808 
+            //            to 9,223,372,036,854,775,807    Com sinal 64-bit integer
+            //ulong   0 to 18,446,744,073,709,551,615     Sem sinal 64-bit integer
 
         }
     }
