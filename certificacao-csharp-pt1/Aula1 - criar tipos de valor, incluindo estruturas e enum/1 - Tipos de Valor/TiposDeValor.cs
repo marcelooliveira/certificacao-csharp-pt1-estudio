9a7c49aac9626ae72fdf7065e7cfb2a8cc23e005
Marcelo Oliveira <mclricardo@gmail.com>
2018-06-19 17:39:19 -0300

    913-CertificacaoCSharpParte1-video1.3-Características dos Tipos de Valor.trec

--- certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/1 - Tipos de Valor/TiposDeValor.cs
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/1 - Tipos de Valor/TiposDeValor.cs
@@ -13,6 +13,16 @@ namespace certificacao_csharp_roteiro
             int idade;
             idade = 30;
             Console.WriteLine(idade);
+
+            int copiaIdade = idade;
+
+            Console.WriteLine($"idade: {idade}");
+            Console.WriteLine($"copiaIdade: {copiaIdade}");
+
+            idade = 23;
+
+            Console.WriteLine($"idade: {idade}");
+            Console.WriteLine($"copiaIdade: {copiaIdade}");
         }
     }
 }
