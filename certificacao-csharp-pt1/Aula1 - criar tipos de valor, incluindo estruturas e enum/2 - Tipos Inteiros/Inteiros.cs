b7b4c42792ffa3a55e972bc01e676aba9d990118
Marcelo Oliveira <mclricardo@gmail.com>
2018-06-19 18:15:03 -0300

    .

--- certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/2 - Tipos Inteiros/Inteiros.cs
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/2 - Tipos Inteiros/Inteiros.cs
@@ -38,35 +38,24 @@ namespace certificacao_csharp_roteiro
 
 
             ///nivelDeAzul = 0xFFFF; // = 65535 em decimal
-            ///Error CS0031  Constant value '65535' cannot be converted to a 'byte'
-            ///
             ///passageirosVoo = 230000; // System.Int16 
-            ///Error CS0031  Constant value '230000' cannot be converted to a 'short'
-            ///
             ///populacao = 15000000000; // System.Int32 
-            ///Error CS0266  Cannot implicitly convert type 'long' to 'int'
-            ///
             ///ushort passageirosNavio = -15; // System.UInt16 
-            ///Error CS0031  Constant value '-15' cannot be converted to a 'ushort'
-            ///
             ///uint estoque = -2300; // System.UInt32 
-            ///Error CS0031  Constant value '-2300' cannot be converted to a 'uint'
-            ///
             ///long populacaoDoBrasil = 207_660_929.345;
-            ///Error CS0266  Cannot implicitly convert type 'double' to 'long'.
 
-            //Tipo    Faixa de valores                    Tamanho
-            //======================================================================
-            //sbyte  -128 to 127                          Com sinal 8-bit integer
-            //byte    0 to 255                            Sem sinal 8-bit integer
-            //char    U+0000 to U+ffff                    Unicode 16-bit character
-            //short  -32,768 to 32,767                    Com sinal 16-bit integer
-            //ushort  0 to 65,535                         Sem sinal 16-bit integer
-            //int     -2,147,483,648 to 2,147,483,647     Com sinal 32-bit integer
-            //uint    0 to 4,294,967,295                  Sem sinal 32-bit integer
-            //long    -9,223,372,036,854,775,808 
-            //            to 9,223,372,036,854,775,807    Com sinal 64-bit integer
-            //ulong   0 to 18,446,744,073,709,551,615     Sem sinal 64-bit integer
+            ///Tipo    Faixa de valores                    Tamanho
+            ///======================================================================
+            ///sbyte  -128 to 127                          Com sinal 8-bit integer
+            ///byte    0 to 255                            Sem sinal 8-bit integer
+            ///char    U+0000 to U+ffff                    Unicode 16-bit character
+            ///short  -32,768 to 32,767                    Com sinal 16-bit integer
+            ///ushort  0 to 65,535                         Sem sinal 16-bit integer
+            ///int     -2,147,483,648 to 2,147,483,647     Com sinal 32-bit integer
+            ///uint    0 to 4,294,967,295                  Sem sinal 32-bit integer
+            ///long    -9,223,372,036,854,775,808 
+            ///            to 9,223,372,036,854,775,807    Com sinal 64-bit integer
+            ///ulong   0 to 18,446,744,073,709,551,615     Sem sinal 64-bit integer
 
         }
     }
