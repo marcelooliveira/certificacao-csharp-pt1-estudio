978ed4a4da8348c69c9ca31e9c6ca798cada68b4
Marcelo Oliveira <mclricardo@gmail.com>
2018-06-20 15:53:12 -0300

    913-CertificacaoCSharpParte1-video1.6-Decimal.trec

--- certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/4 - Decimal/Decimal.cs
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/4 - Decimal/Decimal.cs
@@ -10,36 +10,36 @@ namespace certificacao_csharp_roteiro
     {
         public void Executar()
         {
-            ///double valorProduto1 = 10;
-            ///double valorProduto2 = 20;
-            ///double subtotal      = 30;
+            double valorProduto1 = 10;
+            double valorProduto2 = 20;
+            double subtotal      = 30;
             
-            ///Console.WriteLine("Descobrindo se 10 + 20 == 30");
-            ///Console.WriteLine((valorProduto1 + valorProduto2) == subtotal);
+            Console.WriteLine("Descobrindo se 10 + 20 == 30");
+            Console.WriteLine((valorProduto1 + valorProduto2) == subtotal);
             
-            ///valorProduto1 = 10.1;
-            ///valorProduto2 = 20.2;
-            ///subtotal      = 30.3;
+            valorProduto1 = 10.10;
+            valorProduto2 = 20.20;
+            subtotal      = 30.30;
             
-            ///Console.WriteLine("Descobrindo se 10.1 + 20.2 == 30.3");
-            ///Console.WriteLine((valorProduto1 + valorProduto2) == subtotal);
+            Console.WriteLine("Descobrindo se 10.10 + 20.20 == 30.30");
+            Console.WriteLine((valorProduto1 + valorProduto2) == subtotal);
             
-            ///Console.WriteLine(valorProduto1 + valorProduto2);
+            Console.WriteLine(valorProduto1 + valorProduto2);
             
-            ///Console.WriteLine("Descobrindo se (10.1 + 10.2) == 30.299999999999997");
-            ///Console.WriteLine((valorProduto1 + valorProduto2) == 30.299999999999997);
+            Console.WriteLine("Descobrindo se (10.1 + 10.2) == 30.299999999999997");
+            Console.WriteLine((valorProduto1 + valorProduto2) == 30.299999999999997);
             
-            ///decimal materiaPrima = 10.1;
-            ///decimal maoDeObra    = 20.2;
-            ///decimal custo        = 30.3;
+            decimal materiaPrima = 10.1m;
+            decimal maoDeObra    = 20.2m;
+            decimal custo        = 30.3m;
             
-            ///Console.WriteLine("Descobrindo se (10.1m + 20.2m) == 30.3m");
-            ///Console.WriteLine((materiaPrima + maoDeObra) == custo);
+            Console.WriteLine("Descobrindo se (10.1m + 20.2m) == 30.3m");
+            Console.WriteLine((materiaPrima + maoDeObra) == custo);
             
-            ///Tipo C#     : decimal	
-            ///Tipo .NET   : System.Decimal
-            ///Precisão    : (-7.9 x 10^28 a 7.9 x 10^28) 
-            ///                 28-29 dígitos
+            //Tipo C#     : decimal	
+            //Tipo .NET   : System.Decimal
+            //Precisão    : (-7.9 x 10^28 a 7.9 x 10^28) 
+            //                 28-29 dígitos
         }
     }
 }
