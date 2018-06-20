639e3b48f862530a54f7e1ffed5db74841e6d3f5
Marcelo Oliveira <mclricardo@gmail.com>
2018-06-20 14:52:10 -0300

    913-CertificacaoCSharpParte1-video1.5-Ponto Flutuante.trec

--- certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/3 - Tipos de Ponto Flutuante/PontoFlutuante.cs
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/3 - Tipos de Ponto Flutuante/PontoFlutuante.cs
@@ -10,38 +10,38 @@ namespace certificacao_csharp_roteiro
     {
         public void Executar()
         {
-            ///int idade = 15;
-            ///idade = 15.5;
+            float idade = 15;
+            idade = 15.5f;
             ///
-            ///int massa = 6_000000_000000_000000_000000;
-            ///long massa = 6_000000_000000_000000_000000;
-            ///Console.WriteLine($"long.MinValue: {long.MinValue}");
-            ///Console.WriteLine($"long.MaxValue: {long.MaxValue}");
+            //int massa = 6_000000_000000_000000_000000;
+            //long massa = 6e24;
+            Console.WriteLine($"long.MinValue: {long.MinValue}");
+            Console.WriteLine($"long.MaxValue: {long.MaxValue}");
             ///
-            ///float massaDaTerra = 5.9736e24f; // System.Double
-            ///Console.WriteLine($"massaDaTerra: {massaDaTerra}");
+            float massaDaTerra = 5.9736e24f; // System.Single
+            Console.WriteLine($"massaDaTerra: {massaDaTerra}");
             ///
-            ///float numeroPI = 3.14159f; // System.Single
-            ///Console.WriteLine($"numeroPI: {numeroPI}");
+            float numeroPI = 3.14159f; // System.Single
+            Console.WriteLine($"numeroPI: {numeroPI}");
             ///
-            ///float numeroMuitoMaior = 6e100;
-            ///double numeroMuitoMaior = 6e100;
+            //float numeroMuitoMaior = 6e100f;
+            double numeroMuitoMaior = 6e100;
             ///
-            ///Console.WriteLine();
-            ///Console.WriteLine("Operação com int, float e short");
+            Console.WriteLine();
+            Console.WriteLine("Operação com int, float e short");
+
+            int x = 3;
+            short y = 5;
+
+            var resultado1 = x * y;
+            Console.WriteLine("x * y = {0}", resultado1);
+            Console.WriteLine($"O resultado é do tipo: {resultado1.GetType()}");
             ///
-            ///int x = 3;
-            ///short y = 5;
+            float z = 4.5f;
             ///
-            ///var resultado1 = x * y;
-            ///Console.WriteLine("x * y = {0}", resultado1);
-            ///Console.WriteLine($"O resultado é do tipo: {resultado1.GetType()}");
-            ///
-            ///float z = 4.5f;
-            ///
-            ///var resultado2 = x * y / z;
-            ///Console.WriteLine("x * y / z = {0}", resultado2);
-            ///Console.WriteLine($"O resultado é do tipo: {resultado2.GetType()}");
+            var resultado2 = (x * y) / z;
+            Console.WriteLine("x * y / z = {0}", resultado2);
+            Console.WriteLine($"O resultado é do tipo: {resultado2.GetType()}");
             ///
             ///C#      .NET           Faixa de Valores         Precisão
             ///============================================================
