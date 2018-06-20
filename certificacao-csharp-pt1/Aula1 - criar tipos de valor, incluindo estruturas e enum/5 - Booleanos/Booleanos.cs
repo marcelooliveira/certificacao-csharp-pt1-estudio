9a4f6b078a07ca67021f0536a3dbe9bcadc1116f
Marcelo Oliveira <mclricardo@gmail.com>
2018-06-20 16:17:33 -0300

    913-CertificacaoCSharpParte1-video1.7-Booleanos.trec

--- certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/5 - Booleanos/Booleanos.cs
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/5 - Booleanos/Booleanos.cs
@@ -10,27 +10,27 @@ namespace certificacao_csharp_roteiro
     {
         public void Executar()
         {
-            ///bool possuiSaldo = 1;
+            //bool possuiSaldo = 1;
             
-            ///bool possuiSaldo = true;
+            bool possuiSaldo = true;
             
             /// WriteLine() automaticamente converte o valor de "possuiSaldo" para texto.
-            ///Console.WriteLine($"possuiSaldo: {possuiSaldo}");
+            Console.WriteLine($"possuiSaldo: {possuiSaldo}");
             
-            ///int dias = DateTime.Now.Day;
-            ///Console.WriteLine($"dias: {dias}");
+            int dias = DateTime.Now.Day;
+            Console.WriteLine($"dias: {dias}");
             
-            /// Atribui a diasPar o valor de uma expressão booleana.
-            ///bool diasPar = (dias % 2 == 0);
-            
-            ///if (diasPar == true)
-            ///{
-            ///    Console.WriteLine("dias é um número par");
-            ///}
-            ///else
-            ///{
-            ///    Console.WriteLine("dias é um número ímpar");
-            ///}
+            // Atribui a diasPar o valor de uma expressão booleana.
+            bool diasPar = (dias % 2 == 0);
+
+            if (diasPar)
+            {
+                Console.WriteLine("dias é um número par");
+            }
+            else
+            {
+                Console.WriteLine("dias é um número ímpar");
+            }
         }
     }
 }
