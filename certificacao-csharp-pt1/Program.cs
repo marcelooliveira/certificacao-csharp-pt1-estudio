5f6103367df88743dbf01bfcbb2fdbed298429f6
Marcelo Oliveira <mclricardo@gmail.com>
2018-06-20 14:22:07 -0300

    .

--- certificacao-csharp-pt1/Program.cs
+++ certificacao-csharp-pt1/Program.cs
@@ -66,7 +66,7 @@ namespace certificacao_csharp_roteiro
             {
                 new MenuItem("Tipos de Valor", typeof(TiposDeValor)),
                 new MenuItem("Tipos Inteiros", typeof(TiposInteiros)),
-                //new MenuItem("Ponto Flututante", typeof(PontoFlututante)),
+                new MenuItem("Ponto Flututante", typeof(PontoFlutuante)),
                 //new MenuItem("Decimal", typeof(Decimal)),
                 //new MenuItem("Booleanos", typeof(Booleanos)),
                 //new MenuItem("Estruturas", typeof(Estruturas)),
