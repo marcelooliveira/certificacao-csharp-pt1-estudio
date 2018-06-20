f7f0c5a98b402c584579274b074223d9f22734e4
Marcelo Oliveira <mclricardo@gmail.com>
2018-06-20 15:28:43 -0300

    .

--- certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/4 - Decimal/Decimal.cs
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/4 - Decimal/Decimal.cs
@@ -8,48 +8,38 @@ namespace certificacao_csharp_roteiro
 {
     class Decimal : IAulaItem
     {
-        //The decimal keyword indicates a 128-bit data type.Compared to other floating-point types, the decimal type has more precision and a smaller range, which makes it appropriate for financial and monetary calculations.The approximate range and precision for the decimal type are shown in the following table.
-
-        //Type Approximate Range Precision	.NET Framework type
-        //decimal (-7.9 x 1028 to 7.9 x 1028) / (100 to 1028)	28-29 significant digits    System.Decimal
-        //The default value of a decimal is 0m.
-
-        //Literals
-        //If you want a numeric real literal to be treated as decimal, use the suffix m or M, for example:
-
-        //C#
-
-        //Copy
-        //decimal myMoney = 300.5m;
-        //Without the suffix m, the number is treated as a double and generates a compiler error.
-
-        //Conversions
-        //The integral types are implicitly converted to decimal and the result evaluates to decimal. Therefore you can initialize a decimal variable using an integer literal, without the suffix, as follows:
-
-        //C#
-
-        //Copy
-        //decimal myMoney = 300;
-        //There is no implicit conversion between other floating-point types and the decimal type; therefore, a cast must be used to convert between these two types.For example:
-
-
-        //C#
-
-        //Copy
-        //decimal myMoney = 99.9m;  
-        //double x = (double)myMoney;  
-        //myMoney = (decimal)x;  
-        //You can also mix decimal and numeric integral types in the same expression.However, mixing decimal and other floating-point types without a cast causes a compilation error.
         public void Executar()
         {
-            Console.WriteLine("Descobrindo se (0.1 + 0.2) == 0.3");
-            Console.WriteLine((0.1 + 0.2) == 0.3);
-
-            Console.WriteLine("Descobrindo se (0.1 + 0.2) == 0.30000000000000004");
-            Console.WriteLine((0.1 + 0.2) == 0.30000000000000004);
-
-            Console.WriteLine("Descobrindo se (0.1m + 0.2m) == 0.3m");
-            Console.WriteLine((0.1m + 0.2m) == 0.3m);
+            ///double valorProduto1 = 10;
+            ///double valorProduto2 = 20;
+            ///double subtotal      = 30;
+            
+            ///Console.WriteLine("Descobrindo se 10 + 20 == 30");
+            ///Console.WriteLine((valorProduto1 + valorProduto2) == subtotal);
+            
+            ///valorProduto1 = 10.1;
+            ///valorProduto2 = 20.2;
+            ///subtotal      = 30.3;
+            
+            ///Console.WriteLine("Descobrindo se 10.1 + 20.2 == 30.3");
+            ///Console.WriteLine((valorProduto1 + valorProduto2) == subtotal);
+            
+            ///Console.WriteLine(valorProduto1 + valorProduto2);
+            
+            ///Console.WriteLine("Descobrindo se (10.1 + 10.2) == 30.299999999999997");
+            ///Console.WriteLine((valorProduto1 + valorProduto2) == 30.299999999999997);
+            
+            ///decimal materiaPrima = 10.1;
+            ///decimal maoDeObra = 20.2;
+            ///decimal custo = 30.3;
+            
+            ///Console.WriteLine("Descobrindo se (10.1m + 20.2m) == 30.3m");
+            ///Console.WriteLine((materiaPrima + maoDeObra) == custo);
+            
+            ///Tipo C#     : decimal	
+            ///Tipo .NET   : System.Decimal
+            ///Precisão    : (-7.9 x 10^28 a 7.9 x 10^28) 
+            ///                 28-29 dígitos
         }
     }
 }
