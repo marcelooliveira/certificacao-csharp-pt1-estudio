118dd171c1fcf15c8f0722f140149eef1bfb344d
Marcelo Oliveira <mclricardo@gmail.com>
2018-06-20 16:07:54 -0300

    .

--- certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/5 - Booleanos/Booleanos.cs
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/5 - Booleanos/Booleanos.cs
@@ -10,30 +10,27 @@ namespace certificacao_csharp_roteiro
     {
         public void Executar()
         {
-            //bool possuiSaldo = 1;
-            //Error CS0029  Cannot implicitly convert type 'int' to 'bool'
-
-            bool possuiSaldo = true;
-
-            // WriteLine() automaticamente converte o valor de "possuiSaldo" para texto.
-            Console.WriteLine(possuiSaldo);
-
-            int dias = DateTime.Now.Day;
-            Console.WriteLine($"dias: {dias}");
-
-            // Atribui a diasPar o valor de uma expressão booleana.
-            bool diasPar = (dias % 2 == 0);
-
-            // Executa um braço da condição, dependendo se diasPar for verdadeiro ou falso
-            //if (diasPar == true)
-            if (diasPar)
-                {
-                Console.WriteLine("dias é um número par");
-            }
-            else
-            {
-                Console.WriteLine("dias é um número ímpar");
-            }
+            ///bool possuiSaldo = 1;
+            
+            ///bool possuiSaldo = true;
+            
+            /// WriteLine() automaticamente converte o valor de "possuiSaldo" para texto.
+            ///Console.WriteLine($"possuiSaldo: {possuiSaldo}");
+            
+            ///int dias = DateTime.Now.Day;
+            ///Console.WriteLine($"dias: {dias}");
+            
+            /// Atribui a diasPar o valor de uma expressão booleana.
+            ///bool diasPar = (dias % 2 == 0);
+            
+            ///if (diasPar == true)
+            ///{
+            ///    Console.WriteLine("dias é um número par");
+            ///}
+            ///else
+            ///{
+            ///    Console.WriteLine("dias é um número ímpar");
+            ///}
         }
-}
+    }
 }
