6fa493f296b98c8b72f1779bdaa43737c61da614
Marcelo Oliveira <mclricardo@gmail.com>
2018-06-22 17:09:59 -0300

    .

--- certificacao-csharp-pt1/Program.cs
+++ certificacao-csharp-pt1/Program.cs
@@ -74,7 +74,7 @@ namespace certificacao_csharp_roteiro
                 new MenuItem("Tipos de Refêrencia", typeof(TiposDeReferencia)),
                 new MenuItem("Classes", typeof(Classes)),
                 new MenuItem("Interfaces", typeof(Interfaces)),
-                //new MenuItem("Delegates", typeof(Delegates)),               
+                new MenuItem("Delegates", typeof(Delegates)),               
                 //new MenuItem("Dinâmicos", typeof(Dinamicos)),
                 //new MenuItem("Objetos", typeof(Objetos)),
                 //new MenuItem("Strings", typeof(Strings)),
