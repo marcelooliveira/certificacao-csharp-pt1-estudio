122324a6dbf74dbb379e8024a723ade4cfc1f924
Marcelo Oliveira <mclricardo@gmail.com>
2018-06-22 17:42:07 -0300

    .

--- certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/5 - Dynamic/Dinamicos.cs
+++ certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/5 - Dynamic/Dinamicos.cs
@@ -10,17 +10,6 @@ namespace certificacao_csharp_roteiro
     {
         public void Executar()
         {
-            object objeto = 1;
-            System.Console.WriteLine($"objeto.GetType(): {objeto.GetType()}");
-            //objeto = objeto + 3;
-            //Error CS0019  Operator '+' cannot be applied to operands of type 'object' and 'int'
-
-            dynamic dinamico = 1;
-            System.Console.WriteLine($"dinamico.GetType(): {dinamico.GetType()}");
-
-            dinamico = dinamico + 3;
-            System.Console.WriteLine($"dinamico = dinamico + 3;");
-            Console.WriteLine($"dinamico: {dinamico}");
         }
     }
 }
