7a2f1e8df7c57291041089bf380f43f2f11b4320
Marcelo Oliveira <mclricardo@gmail.com>
2018-06-22 18:44:55 -0300

    913-CertificacaoCSharpParte1-video2.7-Strings.trec

--- certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/7 - Strings/Strings.cs
+++ certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/7 - Strings/Strings.cs
@@ -12,16 +12,16 @@ namespace certificacao_csharp_roteiro
         {
             string a = "bom dia";
             string b = "b";
-            /// Adiciona ao conteúdo de "b"
-            ///b += "om dia";
-            ///Console.WriteLine($"a == b: {a == b}");
-            ///Console.WriteLine($"(object)a == (object)b: {(object)a == (object)b}");
+            // Adiciona ao conteúdo de "b"
+            b = b + "om dia";
+            Console.WriteLine($"a == b: {a == b}");
+            Console.WriteLine($"(object)a == (object)b: {(object)a == (object)b}");
 
-            ///string bomDia = "bom dia";
-            ///Console.WriteLine($"bomDia[4]: {bomDia[4]}");
+            string bomDia = "bom dia";
+            Console.WriteLine($"bomDia[4]: {bomDia[4]}");
 
-            ///var caractere = bomDia[4];
-            ///Console.WriteLine($"caractere.GetType(): {caractere.GetType()}");
+            var caractere = bomDia[4];
+            Console.WriteLine($"caractere.GetType(): {caractere.GetType()}");
         }
     }
 }
