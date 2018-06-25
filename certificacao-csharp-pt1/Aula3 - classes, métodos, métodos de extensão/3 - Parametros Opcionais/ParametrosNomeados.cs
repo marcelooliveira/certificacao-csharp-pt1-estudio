dfddc136756df07460b3543717638f1a7892bc8c
marcelo oliveira <mclricardo@gmail.com>
2018-06-25 17:12:10 -0300

    .

--- certificacao-csharp-pt1/Aula3 - classes, métodos, métodos de extensão/3 - Parametros Opcionais/ParametrosNomeados.cs
+++ certificacao-csharp-pt1/Aula3 - classes, métodos, métodos de extensão/3 - Parametros Opcionais/ParametrosNomeados.cs
@@ -10,20 +10,20 @@ namespace certificacao_csharp_roteiro
     {
         public void Executar()
         {
-            //O método pode ser chamado do jeito normal, usando argumentos posicionais.
-            ImprimirDetalhesDoPedido("Maria de Fátima", 31, "Caneca Vermelha");
-
-            //Argumentos nomeados podem ser fornecidos para os parâmetros em qualquer ordem.
-            ImprimirDetalhesDoPedido(numeroPedido: 31, nomeProduto: "Caneca Vermelha", vendedor: "Maria de Fátima");
-            ImprimirDetalhesDoPedido(nomeProduto: "Caneca Vermelha", vendedor: "Maria de Fátima", numeroPedido: 31);
-
-            //Argumentos nomeados misturados com argumentos posicionais são válidos
-            //desde que sejam usados em sua posição correta.
-            ImprimirDetalhesDoPedido("Maria de Fátima", 31, nomeProduto: "Caneca Vermelha");
-
-            // As 2 linhas abaixo geravam erro de compilação ANTES DO C# 7.2:
-            ImprimirDetalhesDoPedido(vendedor: "Maria de Fátima", 31, nomeProduto: "Caneca Vermelha"); // somente a partir do C# 7.2
-            ImprimirDetalhesDoPedido("Maria de Fátima", numeroPedido: 31, "Caneca Vermelha"); // somente a partir do C# 7.2
+            /// //O método pode ser chamado do jeito normal, usando argumentos posicionais.
+            ///ImprimirDetalhesDoPedido("Maria de Fátima", 31, "Caneca Vermelha");
+            
+            /// //Argumentos nomeados podem ser fornecidos para os parâmetros em qualquer ordem.
+            ///ImprimirDetalhesDoPedido(numeroPedido: 31, nomeProduto: "Caneca Vermelha", vendedor: "Maria de Fátima");
+            ///ImprimirDetalhesDoPedido(nomeProduto: "Caneca Vermelha", vendedor: "Maria de Fátima", numeroPedido: 31);
+            
+            /// //Argumentos nomeados misturados com argumentos posicionais são válidos
+            /// //desde que sejam usados em sua posição correta.
+            ///ImprimirDetalhesDoPedido("Maria de Fátima", 31, nomeProduto: "Caneca Vermelha");
+            
+            /// // As 2 linhas abaixo geravam erro de compilação ANTES DO C# 7.2:
+            ///ImprimirDetalhesDoPedido(vendedor: "Maria de Fátima", 31, nomeProduto: "Caneca Vermelha"); // somente a partir do C# 7.2
+            ///ImprimirDetalhesDoPedido("Maria de Fátima", numeroPedido: 31, "Caneca Vermelha"); // somente a partir do C# 7.2
         }
 
         void ImprimirDetalhesDoPedido(string vendedor, int numeroPedido, string nomeProduto)
@@ -33,7 +33,7 @@ namespace certificacao_csharp_roteiro
                 throw new ArgumentException(message: "Nome de vendedor não pode ser nulo ou vazio.", paramName: nameof(vendedor));
             }
 
-            Console.WriteLine($"Vendedor: {vendedor}, Pedido #: {numeroPedido}, Produto: {nomeProduto}");
+            Console.WriteLine($"Vendedor: {vendedor}, Pedido No.: {numeroPedido}, Produto: {nomeProduto}");
         }
     }
 }
