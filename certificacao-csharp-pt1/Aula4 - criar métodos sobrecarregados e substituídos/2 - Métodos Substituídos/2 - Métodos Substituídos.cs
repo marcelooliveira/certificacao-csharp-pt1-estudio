525cabf87462b2347c72846e19ebe5dee3c27640
marcelooliveira <mclricardo@gmail.com>
2018-06-26 17:05:33 -0300

    .

---
+++ certificacao-csharp-pt1/Aula4 - criar métodos sobrecarregados e substituídos/2 - Métodos Substituídos/2 - Métodos Substituídos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class MetodosSubstituidos : IAulaItem
+    {
+        public void Executar()
+        {
+            Animal gato = new Animal { Nome = "Bichano" };
+            gato.Beber();
+            gato.Comer();
+            gato.Andar();
+        }
+    }
+
+    class Animal
+    {
+        public String Nome { get; set; }
+
+        public void Beber()
+        {
+            Console.WriteLine("Animal.Beber");
+        }
+
+        public void Comer()
+        {
+            Console.WriteLine("Animal.Comer");
+        }
+
+        public void Andar()
+        {
+            Console.WriteLine("Animal.Andar");
+        }
+    }
+}
--- certificacao-csharp-pt1/Program.cs
+++ certificacao-csharp-pt1/Program.cs
@@ -84,7 +84,7 @@ namespace certificacao_csharp_roteiro
                 new MenuItem("Métodos de Extensão", typeof(MetodosDeExtensao)),
                 new MenuItem("Propriedades Indexadas", typeof(PropriedadesIndexadas)),
                 new MenuItem("Sobrecargas", typeof(Sobrecargas)),
-                //new MenuItem("Métodos Substituidos", typeof(MetodosSubstituidos)),
+                new MenuItem("Métodos Substituidos", typeof(MetodosSubstituidos)),
                 //new MenuItem("Boxing", typeof(Boxing)),                
                 //new MenuItem("Unboxing", typeof(Unboxing)),
                 //new MenuItem("Conversoes Implícitas", typeof(ConversoesImplicitas)),
