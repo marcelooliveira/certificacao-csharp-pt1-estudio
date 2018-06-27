2cd85711340a00a562c528e290098ff08244405a
marcelooliveira <mclricardo@gmail.com>
2018-06-27 17:43:32 -0300

    .

--- certificacao-csharp-pt1/Program.cs
+++ certificacao-csharp-pt1/Program.cs
@@ -90,8 +90,8 @@ namespace certificacao_csharp_roteiro
                 new MenuItem("Conversoes Implícitas", typeof(ConversoesImplicitas)),
                 new MenuItem("Conversoes Explícitas", typeof(ConversoesExplicitas)),
                 new MenuItem("Operadores IS e AS", typeof(OperadoresISeAS)),
-                new MenuItem("Operadores de Conversão", typeof(OperadoresDeConversao)),                
-                //new MenuItem("Parse", typeof(Parse)),
+                new MenuItem("Operadores de Conversão", typeof(OperadoresDeConversao)),
+                new MenuItem("Métodos Auxiliares de Conversão", typeof(MetodosAuxiliares)),
                 //new MenuItem("Usando Dynamic", typeof(UsandoDynamic)),
                 //new MenuItem("Conversões de Dynamic", typeof(ConversoesDeDynamic)),
                 //new MenuItem("Resolucão de Sobrecarga", typeof(ResolucaoSobrecarga))
