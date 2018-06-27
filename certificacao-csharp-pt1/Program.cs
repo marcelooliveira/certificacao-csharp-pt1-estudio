0f5d29860561e3b2baf985422b1f7e59a743a5ac
marcelooliveira <mclricardo@gmail.com>
2018-06-27 13:36:48 -0300

    .

--- certificacao-csharp-pt1/Program.cs
+++ certificacao-csharp-pt1/Program.cs
@@ -89,6 +89,7 @@ namespace certificacao_csharp_roteiro
                 new MenuItem("Unboxing", typeof(Unboxing)),
                 new MenuItem("Conversoes Implícitas", typeof(ConversoesImplicitas)),
                 new MenuItem("Conversoes Explícitas", typeof(ConversoesExplicitas)),
+                new MenuItem("Operadores IS e AS", typeof(OperadoresISeAS)),
                 //new MenuItem("Operadores de Conversão", typeof(OperadoresDeConversao)),                
                 //new MenuItem("Parse", typeof(Parse)),
                 //new MenuItem("Usando Dynamic", typeof(UsandoDynamic)),
